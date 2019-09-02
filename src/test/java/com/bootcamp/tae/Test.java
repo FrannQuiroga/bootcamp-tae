9b405f3f0c2a2145ef4e57b5c55f1a8d282844cf
Matias Ceraño <43448876+matiemanuel@users.noreply.github.com>
2019-09-02 18:56:58 -0300

    first push

---
+++ src/test/java/com/bootcamp/tae/Test.java
@@ -0,0 +1,11 @@
+package com.bootcamp.tae;
+
+import com.bootcamp.tae.page.web.WebComponent;
+import com.bootcamp.tae.testsuite.BaseTestSuite;
+import org.junit.Before;
+
+public class Test extends BaseTestSuite {
+
+    
+
+}
