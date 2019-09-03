25bb7bdce93ccd9eddec0ea8b25bbc2bbfb0117b
Nicolas Herrera <nicolasherrera.cp@gmail.com>
2019-09-03 00:06:24 -0300

    clickOnAccomodatios and search for maiami are working, the rest in coming.

--- src/test/java/com/bootcamp/tae/test/TestHome.java
+++ src/test/java/com/bootcamp/tae/test/TestHome.java
@@ -1,14 +1,9 @@
 package com.bootcamp.tae.test;
 
-import com.bootcamp.tae.pages.component.Calendar;
 import com.bootcamp.tae.pages.pageObject.Home;
 import com.bootcamp.tae.pages.pageObject.SearchBox;
 import com.bootcamp.tae.testsuite.BaseTestSuite;
-import com.google.common.html.HtmlEscapers;
-import jdk.internal.util.xml.impl.Input;
 import org.junit.Test;
-import org.openqa.selenium.WebElement;
-import org.w3c.dom.html.HTMLInputElement;
 
 public class TestHome extends BaseTestSuite {
 
